bbca773de5217d6fdb02f3cc19758da4530432b2
Lucas Neves <lucas54neves@gmail.com>
2023-08-25 16:19:42 -0300

    feat: update workstation script

--- scripts/workstation.sh
+++ scripts/workstation.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Update package list
-sudo apt update
+# Downloads directory
+downloadsDirectory="$HOME/Downloads"
+
+# System update
+sudo apt update && sudo apt upgrade -y
+
+if dpkg -l | grep -q wget; then
+    # Notify the user
+    echo "wget is installed."
+else
+    # Notify the user
+    echo "wget is not installed."
+    
+    # Install Wget
+    sudo apt install -y wget
+fi
 
 # Check if VirtualBox is already installed
 if dpkg -l | grep -q virtualbox; then
@@ -11,6 +25,7 @@ fi
 
 # Check if VirtualBox is not installed
 if ! dpkg -l | grep -q virtualbox; then
+    # Notify the user
     echo "VirtualBox is not installed."
     
     # Install required dependencies
@@ -18,50 +33,68 @@ if ! dpkg -l | grep -q virtualbox; then
 
     # Notify the user
     echo "VirtualBox has been installed."
-
+fi
 
 # Check if VSCode is already installed
-if dpkg -l | grep -q code; then
+if which code >/dev/null 2>&1; then
+    # Notify the user
     echo "Visual Studio Code is already installed."
 fi
 
-# Check if VSCode is not installed
-if ! dpkg -l | grep -q code; then
-    # Notify the user
-    echo "Visual Studio Code is not installed."
+if ! which code >/dev/null 2>&1; then
+	# Notify the user
+	echo "Visual Studio Code is not installed."
+	
+    # VSCode url
+	url="https://az764295.vo.msecnd.net/stable/6c3e3dba23e8fadc360aed75ce363ba185c49794/code_1.81.1-1691620686_amd64.deb"
 
-    # Install required dependencies
-    if dpkg -l | grep -q software-properties-common; then
-        echo "software-properties-common is installed."
-    else
-        echo "software-properties-common is not installed."
-        sudo apt install -y software-properties-common
-    fi
-
-    if dpkg -l | grep -q apt-transport-https; then
-        echo "apt-transport-https is installed."
-    else
-        echo "apt-transport-https is not installed."
-        sudo apt install -y apt-transport-https
-    
-    if dpkg -l | grep -q wget; then
-        echo "wget is installed."
-    else
-        echo "wget is not installed."
-        sudo apt install -y wget
-
-    # Add the Microsoft GPG key
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
-
-    # Add the Visual Studio Code repository
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-
-    # Update package list again
-    sudo apt update
-
-    # Install Visual Studio Code
-    sudo apt install -y code
-
-    # Notify the user
-    echo "Visual Studio Code has been installed."
+    # Download deb file
+	wget -c "$url" -O "$downloadsDirectory/vscode.deb"
+	
+	# Notify the user
+	echo "Visual Studio Code will be installed."
 fi
+
+# Check if Google Chrome is already installed
+if dpkg -l | grep -q chrome; then
+    # Notify the user
+    echo "Google Chrome is already installed."
+fi
+
+if ! dpkg -l | grep -q chrome; then
+	# Notify the user
+	echo "Google Chrome is not installed."
+	
+    # Google Chrome url
+	url="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
+    # Download deb file
+	wget -c "$url" -O "$downloadsDirectory/chrome.deb"
+	
+	# Notify the user
+	echo "Google Chrome will be installed."
+fi
+
+# Install External Programs
+for file in "$downloadsDirectory"/*.deb; do
+	if [ -e "$file" ]; then
+		# Install package
+        sudo dpkg -i $downloadsDirectory/*.deb
+
+        # Notify the user
+		echo "Installed: $file"
+        
+        # Remove file
+		rm "$file"
+
+        # Notify the user
+		echo "Removed: $file"
+	fi
+done
+
+# Forcefully installs packages without asking for confirmation and automatically resolves broken dependencies
+sudo apt install -fy
+
+# Notify the user
+echo "External programs installed"
+
