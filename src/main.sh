4b013bc73f3a53b2bc77f1b38150b7cbf3eb2dbb
Lucas Neves <lucas54neves@gmail.com>
2025-09-27 17:31:41 -0300

    feat: add NVM installation to main script

--- src/main.sh
+++ src/main.sh
@@ -52,6 +52,13 @@ installDebFiles() {
 	rm -v "$DOWNLOAD_DIRECTORY"/*.deb
 }
 
+installNvm() {
+	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+	export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
 installAnki() {
 	EXTRACT_DIRECTORY="$DOWNLOAD_DIRECTORY/anki-launcher"
 
@@ -76,4 +83,5 @@ installAnki() {
 updateSystem
 installAptPackages
 installDebFiles
+installNvm
 installAnki
